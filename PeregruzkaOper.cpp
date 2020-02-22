6c52fc9c644c8df2bb0326a171ef1e2e5cfd0389
Hukuma5 <56114535+Hukuma5@users.noreply.github.com>
2020-02-22 10:15:59 +0300

    ff

--- PeregruzkaOper.cpp
+++ PeregruzkaOper.cpp
@@ -7,5 +7,20 @@ class Vector
 	int n;
 	double* a;
 public:
+	/*Vector()
+	{
+		int m = 0;
+		double* p = new double[m];
+		p[0] = 0;fff
+	}*/
+	Vector(double* a, int n)
+	{
+		this->n = n;
+		this->a = new double[n];
+		for (int i = 0; i < n; i++)
+		{
+			this->a[i] = a[i];
+		}
+	}
 
 };
